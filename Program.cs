cd75d6b6c5a435d55788b20dfbc0602299984d35
John Farrell <jfarrell@caci.co.uk>
2020-11-09 15:26:02 +0000

    init empty command line c# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poker_monte_carlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
